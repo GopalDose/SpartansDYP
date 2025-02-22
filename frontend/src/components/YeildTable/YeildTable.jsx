f75e5277bc35b52571ed89536ac38acc9f33d397
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-02-22 18:27:25 +0530

    Add crop form

--- frontend/src/components/YeildTable/YeildTable.jsx
+++ frontend/src/components/YeildTable/YeildTable.jsx
@@ -9,6 +9,35 @@ const YeildTable = () => {
       <Navbar />
       <div className="yeild">
         <h1>Wheat</h1>
+        <div className="data">Created Date: 10/01/2021</div>
+        <table className="table">
+          <tr>
+            <div  className='newentry'>
+              + Add New Entry
+            </div>
+          </tr>
+          <tr className='data'>
+            <div className="sr">1 <div className="typename">Sowing</div></div>
+            <div className="price">30000 <button>view</button></div>
+          </tr>
+          <tr className='data'>
+            <div className="sr">1 <div className="typename">Sowing</div></div>
+            <div className="price">30000 <button>view</button></div>
+          </tr>
+          <tr className='data'>
+            <div className="sr">1 <div className="typename">Sowing</div></div>
+            <div className="price">30000 <button>view</button></div>
+          </tr>
+          <tr className='data'>
+            <div className="sr">1 <div className="typename">Sowing</div></div>
+            <div className="price">30000 <button>view</button></div>
+          </tr>
+          <tr className='data'>
+            <div className="sr">1 <div className="typename">Sowing</div></div>
+            <div className="price">30000 <button>view</button></div>
+          </tr>
+
+        </table>
       </div>
       <Footer />
     </>
