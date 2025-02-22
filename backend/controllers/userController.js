7ef0154fc969ce4ff7e2fa1ea10e43bfa5bde3c3
shreeshail-chavan <shreeshailchavan309@gmail.com>
2025-02-22 20:29:58 +0530

    user registration checking compelte

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,22 +24,26 @@ exports.getUserById = async (req, res) => {
 // Create a new user
 exports.createUser = async (req, res) => {
   try {
-    const { username, password ,name} = req.body;
+    const { username, password, name } = req.body;
+
     if (!username || !password)
       return res
         .status(400)
         .json({ message: "Userame and Passowrd are required" });
-    
-    const newUser = new User({ username, password ,name});
+    const user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(200)
+        .json({ message: "Username already exists", exists: true });
+    }
+    const newUser = new User({ username, password, name });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: "Error creating user , User is already registered",
-        error,
-      });
+    res.status(500).json({
+      message: "Error creating user , User is already registered",
+      error,
+    });
   }
 };
 
@@ -47,12 +51,15 @@ exports.userProfile = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const updateUserDetails = req.body;
-    const {adharno,panno} = req.body
-    const adharres = user.findOne({adhaarno:adharno})
-    const panres = user.findOne({panno:panno})
-    if(adharres || panres) return res.status(400).json({message:"User with adhaar or panno already exits"})
+    const { adharno, panno } = req.body;
+    const adharres = user.findOne({ adhaarno: adharno });
+    const panres = user.findOne({ panno: panno });
+    if (adharres || panres)
+      return res
+        .status(400)
+        .json({ message: "User with adhaar or panno already exits" });
     if (!user) return res.status(404).json({ message: "User not found" });
-    user.updateOne(updateUserDetails)
+    user.updateOne(updateUserDetails);
   } catch (error) {}
 };
 
